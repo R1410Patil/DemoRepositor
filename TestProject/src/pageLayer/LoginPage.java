3b31ca830e6ded8b3f5d083c72fbc432a65eedd1
R1410Patil <rutujapatiltesting@gmail.com>
2023-03-05 21:30:01 +0530

    Login Page commit

---
+++ TestProject/src/pageLayer/LoginPage.java
@@ -0,0 +1,12 @@
+package pageLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("click on login | home page will get open");
+
+	}
+
+}
--- TestProject/src/testPackage/TestClass.java
+++ TestProject/src/testPackage/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[]args) {
 		
-		System.out.println("TestBase class");
+		System.out.println("Base class");
 		System.out.println("Util class");
 		System.out.println("PageLayer class");
 		System.out.println("TestLayer class");
